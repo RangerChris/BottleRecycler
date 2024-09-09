6fe98aacb1826580e86e128d3224ba7027692d85
Kresten Elleby <elleby@kresten.dk>
2024-09-09 23:13:32 +0200

    New base url

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-const baseUrl = "http://localhost:5173/";
 test("has title", async ({ page }) => {
-  await page.goto(baseUrl);
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Bottle Recycler/);
