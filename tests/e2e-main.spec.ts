15ec38a289b5795443cc71835b2906f5c8044b74
Kresten Elleby <elleby@kresten.dk>
2024-10-03 23:28:12 +0200

    Fix UI test

--- tests/e2e-main.spec.ts
+++ tests/e2e-main.spec.ts
@@ -8,7 +8,8 @@ test("Go to main page", async ({ page }) => {
 test("Add a recycler and start it", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle(/Bottle Recycler/);
-  await page.getByLabel("$").click();
+  await page.getByRole("button", { name: "Start game" }).click();
+  await page.getByRole("button", { name: "Buy recycler" }).click();
   await expect(page.locator("#root")).toContainText("Recycler 1");
   await expect(page.getByRole("list")).toContainText("Stopped");
   await page.getByLabel("Starts or stops the recycler").click();
